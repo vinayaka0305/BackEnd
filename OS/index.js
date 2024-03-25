507662bbedc475283c82c553eb3e47a97ad01831
vinayaka0305 <vav6473@gmail.com>
2024-03-25 13:17:07 +0530

    code

--- OS/index.js
+++ OS/index.js
@@ -1,6 +1,8 @@
 const global = require("os");
+//require is for importing 
 
 let platForm = global.platform()
+//planform() is inbuild function that checks of os plat form
 console.log(platForm);
 
 // to check the operating system we have use code modules inbuilt package os
