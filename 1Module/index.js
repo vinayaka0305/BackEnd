6c35240c03ca96653abff540b4cd675d62964394
vinayaka0305 <vav6473@gmail.com>
2024-03-25 12:13:34 +0530

    code

---
+++ 1Module/index.js
@@ -0,0 +1,11 @@
+//to import from the helper file
+
+const global = require("./helper")
+// we have to store the fileName in varibel and by using require keyword;
+
+console.log(global.add(3,3));
+console.log(global.sub(10,5));
+// like this we can able to call that functions and we can pass parameter here;
+// to run this we have put that particular file where we are calling and consoling
+//here index is the file name 
+
